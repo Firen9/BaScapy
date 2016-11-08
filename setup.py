64e9b97f8210165fdd1a5da14d431d30224781e7
sascha <sascha@sascha.de>
2016-11-08 12:46:58 +0000

    Setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+
+setup(
+    name='BaScapy',
+    version='',
+    packages=['scapy-2.3.2.build.lib.linux-x86_64-2.7.scapy', 'scapy-2.3.2.build.lib.linux-x86_64-2.7.scapy.arch',
+              'scapy-2.3.2.build.lib.linux-x86_64-2.7.scapy.arch.windows',
+              'scapy-2.3.2.build.lib.linux-x86_64-2.7.scapy.asn1', 'scapy-2.3.2.build.lib.linux-x86_64-2.7.scapy.tools',
+              'scapy-2.3.2.build.lib.linux-x86_64-2.7.scapy.crypto',
+              'scapy-2.3.2.build.lib.linux-x86_64-2.7.scapy.layers',
+              'scapy-2.3.2.build.lib.linux-x86_64-2.7.scapy.contrib',
+              'scapy-2.3.2.build.lib.linux-x86_64-2.7.scapy.modules', 'scapy-2.3.2.scapy', 'scapy-2.3.2.scapy.arch',
+              'scapy-2.3.2.scapy.arch.windows', 'scapy-2.3.2.scapy.asn1', 'scapy-2.3.2.scapy.tools',
+              'scapy-2.3.2.scapy.crypto', 'scapy-2.3.2.scapy.layers', 'scapy-2.3.2.scapy.contrib',
+              'scapy-2.3.2.scapy.modules'],
+    url='',
+    license='',
+    author='Sascha Böhm',
+    author_email='',
+    description=''
+)
